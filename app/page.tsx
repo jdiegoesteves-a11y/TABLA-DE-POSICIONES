9c6f114bba3799807194a6df0543acfe460dc817
jdiegoesteves-a11y <jdiegoesteves@gmail.com>
2026-03-11 21:48:11 -0500

    Moviendo tabla a la pagina principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { db } from "../../lib/firebase";
+import { db } from "../lib/firebase";
 import { collection, onSnapshot } from "firebase/firestore";
 
 export default function Tabla() {
